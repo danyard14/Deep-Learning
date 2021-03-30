c62e4f82891c43005ecd1e9b234cc44f77a2a43e
MIDDLEEAST\t-ofermoses <t-ofermoses@microsoft.com>
2021-03-30 13:47:49 +0300

    with grad test

--- functions.py
+++ functions.py
@@ -1,5 +1,8 @@
 import numpy as np
 from numpy import log, exp, divide
+import matplotlib.pyplot as plt
+import h5py
+import scipy.io as sio
 
 
 def get_ettas(X, W, m):
@@ -11,6 +14,51 @@ def get_ettas(X, W, m):
     return ettas
 
 
+# def get_class_vec(Y_vecs):
+#     l, m = Y_vecs.shape
+#     Y = np.zeros(m)
+#     for i in m:
+#         np.argmax()
+
+def normalize(v):
+    norm = np.linalg.norm(v)
+    if norm == 0:
+        return v
+    return v / norm
+
+
+def grad_test_soft_max(X: np.array, Y: np.array, W: np.array):
+    """
+       description..
+       :param X: a matrix of nxm where n is the dimension of x and m is the number of examples x
+       :param Y: a vector of size 1xm, where Y[i] is in [1,...,l]
+       :param W: a vector nx1 where n is the dimension of x
+       :return: int loss
+       """
+    iter_num = 20
+    diff = np.zeros(iter_num)
+    diff_grad = np.zeros(iter_num)
+    epsilons = [0.5 ** i for i in range(iter_num)]
+    n, m = X.shape
+    d = normalize(np.random.rand(n))
+    fw, grads = soft_max_regression(X, Y, W)
+    grad = grads[:, 0]  # grad of w_0
+    for i, epsilon in enumerate(epsilons):
+        W_diff = W.copy()
+        W_diff[:, 0] += d * epsilon
+        fw_epsilon = soft_max_regression(X, Y, W_diff)[0]
+        diff[i] = abs(fw_epsilon - fw)
+        diff_grad[i] = abs(fw_epsilon - fw - epsilon * d.T @ grad)
+
+    plt.semilogy(np.arange(1, iter_num + 1, 1), diff)
+    plt.semilogy(np.arange(1, iter_num + 1, 1), diff_grad)
+    plt.xlabel('epsilons')
+    plt.ylabel('difference')
+    plt.title('Grad Test Results')
+    plt.legend(("diff without grad", "diff with grad"))
+    plt.show()
+
+
 def soft_max_regression(X: np.array, Y: np.array, W: np.array):
     """
     description..
@@ -20,19 +68,17 @@ def soft_max_regression(X: np.array, Y: np.array, W: np.array):
     :return: int loss
     """
     # TODO:
-    #  1. add etta to calculation
     #  2. add bias
 
-    m = X.shape[1]
+    n, m = X.shape
     l = W.shape[1]
 
     X_t = X.T
     loss = 0
     right_sum = 0
     ettas_vector = get_ettas(X, W, m)
-    grads = np.zeros((m, l))   # each column j will be the grad with respect to w_j
+    grads = np.zeros((n, l))  # each column j will be the grad with respect to w_j
     # TODO: check if grads is not nxl (w has dimension nx1)
-
 
     for j in range(l):
         w_j = W[:, j]
@@ -40,13 +86,20 @@ def soft_max_regression(X: np.array, Y: np.array, W: np.array):
 
     for k in range(l):
         w_k = W[:, k]
-        c_k = np.array([1 if y == k else 0 for y in Y])
+        c_k = Y[k, :]
         diag_v_inv_u = divide(exp(X_t @ w_k - ettas_vector), right_sum)
         loss += c_k.T @ log(diag_v_inv_u)
-        grads[:, k] = (1/m) * X @ (diag_v_inv_u - c_k)
+        grads[:, k] = (1 / m) * X @ (diag_v_inv_u - c_k)
 
     return - loss / m, grads
 
 
 if __name__ == '__main__':
-    pass
+    data = sio.loadmat('SwissRollData.mat')
+    Y_train = data["Ct"]
+    Y_validation = data["Cv"]
+    X_train = data["Yt"]
+    X_validation = data["Yv"]
+    w = np.array([[1, -1], [0.5, 2]])
+    grad_test_soft_max(X_train, Y_train, w)
+    plt.show()
