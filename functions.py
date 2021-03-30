48bdf4ea5f4a19594f9b9f859509e2d30ca1ace4
MIDDLEEAST\t-ofermoses <t-ofermoses@microsoft.com>
2021-03-30 17:03:07 +0300

    added bias?

--- functions.py
+++ functions.py
@@ -5,11 +5,11 @@ import h5py
 import scipy.io as sio
 
 
-def get_ettas(X, W, m):
+def get_ettas(X, W, m, b):
     ettas = np.zeros(m)
     for j in range(m):
         x_j = X[:, j]
-        candidates = x_j @ W
+        candidates = x_j @ W + b
         ettas[j] = np.max(candidates)
     return ettas
 
@@ -59,12 +59,13 @@ def grad_test_soft_max(X: np.array, Y: np.array, W: np.array):
     plt.show()
 
 
-def soft_max_regression(X: np.array, Y: np.array, W: np.array):
+def soft_max_regression(X: np.array, Y: np.array, W: np.array, b: np.array):
     """
     description..
     :param X: a matrix of nxm where n is the dimension of x and m is the number of examples x
-    :param Y: a vector of size 1xm, where Y[i] is in [1,...,l]
+    :param Y: a matrix of size lxm, where Y[i,:] is c_i (indicator vector for label i)
     :param W: a matrix nxl where n is the dimension of x and l is the number of labels
+    :param b: a vector of size 1*l, where b[i] is the bias of w_i
     :return: int loss
     """
     # TODO:
@@ -76,22 +77,70 @@ def soft_max_regression(X: np.array, Y: np.array, W: np.array):
     X_t = X.T
     loss = 0
     right_sum = 0
-    ettas_vector = get_ettas(X, W, m)
-    grads = np.zeros((n, l))  # each column j will be the grad with respect to w_j
+    ettas_vector = get_ettas(X, W, m, b)
+    W_grads = np.zeros((n, l))  # each column j will be the grad with respect to w_j
+    b_grad = np.zeros(l)
     # TODO: check if grads is not nxl (w has dimension nx1)
 
     for j in range(l):
         w_j = W[:, j]
-        right_sum += exp(X_t @ w_j - ettas_vector)
+        right_sum += exp(X_t @ w_j + b[j] - ettas_vector)
 
     for k in range(l):
         w_k = W[:, k]
         c_k = Y[k, :]
-        diag_v_inv_u = divide(exp(X_t @ w_k - ettas_vector), right_sum)
+        diag_v_inv_u = divide(exp(X_t @ w_k + b[k] - ettas_vector), right_sum)
         loss += c_k.T @ log(diag_v_inv_u)
-        grads[:, k] = (1 / m) * X @ (diag_v_inv_u - c_k)
+        W_grads[:, k] = (1 / m) * X @ (diag_v_inv_u - c_k)
 
-    return - loss / m, grads
+    return - loss / m, W_grads
+
+
+def get_acc(X, Y, W):
+    XW = X.T @ W
+    preds = np.argmax(XW, axis=1)
+    true_labels = np.argmax(Y, axis=0)
+    acc = sum(preds != true_labels)
+    return acc / Y.shape[1]
+
+
+def SGD(X, Y, W, lr=0.1, epchos=100, batch_size=32):
+    m = X.shape[1]
+    losses = np.zeros(epchos)
+    accuracy = np.zeros(epchos)
+    for i in range(epchos):
+        if i % 25 == 0:
+            lr /= 10
+        perm_indices = np.random.permutation(m)
+        loss = 0
+        for j in range(0, m, batch_size):
+            X_batch = X[:, perm_indices[j:j + batch_size]]
+            Y_batch = Y[:, perm_indices[j:j + batch_size]]
+            loss, grad = soft_max_regression(X_batch, Y_batch, W)
+            W -= lr * grad
+            bias
+        losses[i] = loss
+        accuracy[i] = get_acc(X, Y, W)
+        ## validate
+
+        print(f"epcoch = {i}, loss = {loss}, accuracy = {accuracy[i]} grad = {grad}")
+
+    plt.plot(np.arange(0, epchos, 1), accuracy)
+    plt.plot(np.arange(0, epchos, 1), losses)
+    plt.show()
+    return losses, W
+
+
+def run_grad_test_soft_max():
+    w = np.array([[1, -1], [0.5, 2]])
+    return grad_test_soft_max(X_train, Y_train, w)
+
+
+def test_SGD(X, Y):
+    # W = np.ones((2, 2))
+    W = np.random.random((2, 2))
+
+    losses, W = SGD(X, Y, W)
 
 
 if __name__ == '__main__':
@@ -100,6 +149,5 @@ if __name__ == '__main__':
     Y_validation = data["Cv"]
     X_train = data["Yt"]
     X_validation = data["Yv"]
-    w = np.array([[1, -1], [0.5, 2]])
-    grad_test_soft_max(X_train, Y_train, w)
-    plt.show()
+    # run_grad_test_soft_max()
+    test_SGD(X_train, Y_train)
