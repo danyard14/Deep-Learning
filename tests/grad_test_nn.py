d2bb72d7930120c76d8edcfbddf8115b22e81a31
MIDDLEEAST\t-ofermoses <t-ofermoses@microsoft.com>
2021-04-19 18:01:11 +0300

    updated pdf

--- tests/grad_test_nn.py
+++ tests/grad_test_nn.py
@@ -48,6 +48,7 @@ def grad_test_soft_max_weights_nn(X: np.array, Y: np.array, iter_num=20):
                 "|\N{LATIN SMALL LETTER F WITH HOOK}(x + εd) - \N{LATIN SMALL LETTER F WITH HOOK}(x) - εdᵀ∇f|"))
     plt.show()
 
+
 def grad_test_soft_max_bias_nn(X: np.array, Y: np.array, iter_num=20):
     """
     Gradiant test for bias parameter
@@ -94,45 +95,6 @@ def grad_test_soft_max_bias_nn(X: np.array, Y: np.array, iter_num=20):
     plt.show()
 
 
-def grad_test_soft_max_bias(X: np.array, Y: np.array, iter_num=20):
-    """
-    Gradiant test for bias parameter
-    :param X: a matrix of size nxm, output of forward layer
-    :param Y: a matrix of size lxm, where Y[i,:] is c_i (indicator vector for label i)
-    :param iter_num: a number of iterations for the gradient test
-    """
-    diff = np.zeros(iter_num)
-    diff_grad = np.zeros(iter_num)
-    epsilons = [0.5 ** i for i in range(iter_num)]
-    n, m = X.shape
-    l = Y.shape[0]
-    net = NeuralNetwork(n, 4, l, policy="increase")
-    soft_max_layer = net.soft_max_layer
-    b_original = soft_max_layer.b.copy()
-    d = normalize(np.random.rand(*soft_max_layer.b.shape))
-    out = net.forward_pass(X)
-    fb, _ = soft_max_layer.soft_max(out, Y)
-    grad_b = soft_max_layer.db
-    for i, epsilon in enumerate(epsilons):
-        b_diff = b_original.copy()
-        b_diff += d * epsilon
-        soft_max_layer.b = b_diff
-        out_epsilon = net.forward_pass(X)
-        fb_epsilon, _ = soft_max_layer.soft_max(out_epsilon, Y)
-        diff[i] = abs(fb_epsilon - fb)
-        diff_grad[i] = abs(fb_epsilon - fb - epsilon * d.T @ grad_b)
-
-    plt.semilogy(np.arange(1, iter_num + 1, 1), diff)
-    plt.semilogy(np.arange(1, iter_num + 1, 1), diff_grad)
-    plt.xlabel('epsilons')
-    plt.ylabel('difference')
-    plt.title('bias Grad Test Results - whole network')
-    plt.legend((f'|\N{LATIN SMALL LETTER F WITH HOOK}(x + εd) - \N{LATIN SMALL LETTER F WITH HOOK}(x)|',
-                "|\N{LATIN SMALL LETTER F WITH HOOK}(x + εd) - \N{LATIN SMALL LETTER F WITH HOOK}(x) - εdᵀ∇f|"))
-    plt.show()
-
-
 if __name__ == '__main__':
-    #grad_test_soft_max_weights_nn(*get_grad_Jac_test_params())
+    # grad_test_soft_max_weights_nn(*get_grad_Jac_test_params())
     grad_test_soft_max_bias_nn(*get_grad_Jac_test_params())
-
